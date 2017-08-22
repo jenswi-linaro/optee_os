613edaaeadd5851b709a0e24f47fd54ed1379b95
Jens Wiklander <jens.wiklander@linaro.org>
2017-08-22 16:06:24 +0200

    [review] core: add MEM_AREA_PAGER_VASPACE
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/mm/core_mmu.c
+++ core/arch/arm/mm/core_mmu.c
@@ -694,18 +694,19 @@ static void dump_mmap_table(struct tee_mmap_region *memory_map)
 }
 
 static void add_pager_vaspace(struct tee_mmap_region *mmap __maybe_unused,
-			      size_t num_elems __maybe_unused,
-			      vaddr_t begin __maybe_unused,
+			      size_t num_elems __maybe_unused, vaddr_t begin,
 			      vaddr_t end __maybe_unused,
-			      size_t *last __maybe_unused)
+			      size_t *last __maybe_unused,
+			      vaddr_t *next_pgdir_va)
 {
+	vaddr_t begin_pgdir = ROUNDDOWN(begin, CORE_MMU_PGDIR_SIZE);
 #ifdef CFG_WITH_PAGER
-	size_t size = CFG_TEE_RAM_VA_SIZE - (end - begin);
+	size_t size = CFG_TEE_RAM_VA_SIZE - (end - begin_pgdir);
 	size_t n;
 	size_t pos = 0;
 
 	if (!size)
-		return;
+		goto out;
 
 	if (*last >= (num_elems - 1)) {
 		EMSG("Out of entries (%zu) in memory map", num_elems);
@@ -726,7 +727,11 @@ static void add_pager_vaspace(struct tee_mmap_region *mmap __maybe_unused,
 	mmap[pos].size = size;
 	mmap[pos].region_size = SMALL_PAGE_SIZE;
 	mmap[pos].attr = core_mmu_type_to_attr(MEM_AREA_PAGER_VASPACE);
-#endif
+out:
+#endif /*CFG_WITH_PAGER*/
+
+	*next_pgdir_va = ROUNDUP(begin_pgdir + CFG_TEE_RAM_VA_SIZE,
+				 CORE_MMU_PGDIR_SIZE);
 }
 
 static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
@@ -738,6 +743,7 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	vaddr_t va;
 	vaddr_t __maybe_unused end;
 	bool __maybe_unused va_is_secure = true; /* any init value fits */
+	vaddr_t next_pgdir_va;
 
 	for (mem = &__start_phys_mem_map_section;
 	     mem < &__end_phys_mem_map_section; mem++) {
@@ -837,12 +843,14 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	assert(va >= CFG_TEE_RAM_START);
 	assert(end <= CFG_TEE_RAM_START + CFG_TEE_RAM_VA_SIZE);
 
-	add_pager_vaspace(memory_map, num_elems, va, end, &last);
+	add_pager_vaspace(memory_map, num_elems, va, end, &last,
+			  &next_pgdir_va);
 
 	if (core_mmu_place_tee_ram_at_top(va)) {
 		/* Map non-flat mapped addresses below flat mapped addresses */
 		for (map = memory_map; !core_mmap_is_end_of_table(map); map++) {
-			if (map_is_flat_mapped(map))
+			if (map_is_flat_mapped(map) ||
+			    map->type == MEM_AREA_PAGER_VASPACE)
 				continue;
 
 #if !defined(CFG_WITH_LPAE)
@@ -862,7 +870,7 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 		}
 	} else {
 		/* Map non-flat mapped addresses above flat mapped addresses */
-		va = ROUNDUP(va + CFG_TEE_RAM_VA_SIZE, CORE_MMU_PGDIR_SIZE);
+		va = next_pgdir_va;
 		for (map = memory_map; !core_mmap_is_end_of_table(map); map++) {
 			if (map_is_flat_mapped(map) ||
 			    map->type == MEM_AREA_PAGER_VASPACE)
