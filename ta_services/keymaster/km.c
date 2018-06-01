0b720232a04dc1bee350dca3d614bed62135c3f6
Igor Opaniuk <igor.opaniuk@linaro.org>
2018-06-01 13:50:52 +0300

    ta_services: keymaster: implement km_add_rng_entropy
    Add implementation of km_add_rng_entropy call, which is used for seeding
    RNG enropy pool.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>

--- ta_services/keymaster/km.c
+++ ta_services/keymaster/km.c
@@ -1,12 +1,46 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /* Copyright (c) 2018, Linaro Limited */
 
+#include <pta_system.h>
 #include <tee_internal_api.h>
 
 #include "km.h"
 
-TEE_Result km_add_rng_entropy(const void __unused *buf, size_t __unused blen)
+TEE_Result km_add_rng_entropy(const void *buf, size_t blen)
 {
-	/* Stubbed until the system PTA is available */
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	static const TEE_UUID system_uuid = PTA_SYSTEM_UUID;
+	TEE_TASessionHandle sess = TEE_HANDLE_NULL;
+	TEE_Result res;
+	uint32_t pt;
+	TEE_Param params[4] = { { {0} } };
+	uint32_t ret_orig;
+
+	if (!blen)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = TEE_OpenTASession(&system_uuid, 0, 0, NULL, &sess, &ret_orig);
+	if (res != TEE_SUCCESS) {
+		EMSG("TEE_OpenTASession to System Pseudo TA failed");
+		return res;
+	}
+
+	pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+				TEE_PARAM_TYPE_NONE,
+				TEE_PARAM_TYPE_NONE,
+				TEE_PARAM_TYPE_NONE);
+	params[0].memref.buffer = (void *)buf;
+	params[0].memref.size = blen;
+
+	res = TEE_InvokeTACommand(sess, 0, PTA_SYSTEM_ADD_RNG_ENTROPY,
+				  pt, params, &ret_orig);
+	if (res != TEE_SUCCESS) {
+		EMSG("TEE_InvokeTACommand to System Pseudo TA failed");
+		goto cleanup_return;
+	}
+
+
+cleanup_return:
+	TEE_CloseTASession(sess);
+	return res;
 }
+
