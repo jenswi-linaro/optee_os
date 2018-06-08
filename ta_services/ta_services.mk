jenswi-linaro/optee_os

commit c7542534816cdbb41745b642196083c1ca8a44a8

mk: add ta_services.mk
Add initial ta_services.mk, which handles build targets for each TA in
ta_services.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>

ta_services/ta_services.mk
@@ -1,0 +1,43 @@
+.PHONY: all
+all: ta_dev_kit
+
+.PHONY: clean
+clean:
+
+ta_flags_32 := CROSS_COMPILE="$(CROSS_COMPILE32)" \
+	    TA_DEV_KIT_DIR="$(CURDIR)/$(out-dir)/export-ta_arm32"
+
+ta_flags_64 := CROSS_COMPILE="$(CROSS_COMPILE64)" \
+	    TA_DEV_KIT_DIR="$(CURDIR)/$(out-dir)/export-ta_arm64"
+
+# Keymaster TA
+ifeq ($(CFG_TA_SERVICES_KEYMASTER),32)
+build_ta_keymaster := y
+ta_keymaster_flags := $(ta_flags_32)
+else ifeq ($(CFG_TA_SERVICES_KEYMASTER),64)
+build_ta_keymaster := y
+# if core is built as 32, forcing TA to be built also as 32bit
+ifneq ($(CFG_ARM64_core),y)
+ta_keymaster_flags := $(ta_flags_32)
+else
+ta_keymaster_flags := $(ta_flags_64)
+endif
+else
+build_ta_keymaster := n
+endif
+
+ifeq ($(build_ta_keymaster),y)
+ta_keymaster_dir := keymaster
+
+all: ta_keymaster
+.PHONY: ta_keymaster
+ta_keymaster:
+	$(MAKE) -C ta_services/$(ta_keymaster_dir) $(ta_keymaster_flags) \
+		O=$(CURDIR)/$(out-dir)/ta_services/$(ta_keymaster_dir)
+
+clean: ta_keymaster_clean
+.PHONY: ta_keymaster_clean
+ta_keymaster_clean:
+	$(MAKE) -C ta_services/$(ta_keymaster_dir) $(ta_keymaster_flags) \
+		O=$(CURDIR)/$(out-dir)/ta_services/$(ta_keymaster_dir) clean
+endif

Makefile
@@ -83,6 +83,10 @@ endef
 $(foreach t, $(ta-targets), $(eval $(call build-ta-target, $(t))))
 endif
 
+ifeq ($(CFG_TA_SERVICES),y)
+include ta_services/ta_services.mk
+endif
+
 include mk/cleandirs.mk
 
 .PHONY: clean

mk/config.mk
@@ -326,3 +326,9 @@ CFG_TA_MBEDTLS ?= y
 # Compile the TA library mbedTLS with self test functions, the functions
 # need to be called to test anything
 CFG_TA_MBEDTLS_SELF_TEST ?= y
+
+# Compile additional TAs that provide misc. auxiliary services
+CFG_TA_SERVICES ?= y
+
+# Enable Keymaster TA, which provides AOSP Keymaster services.
+CFG_TA_SERVICES_KEYMASTER ?= 64

commit 2167d3fdcbcdd859b16fe3167ad90bcb74a93abd

ta_keymaster target depends on ta_dev_kit
The ta_keymaster need to depend on ta_dev_kit for parallel make to work
properly. Fixes errors like:
make[1]: Entering directory '.../optee_os/ta_services/keymaster'
.../export-ta_arm32/mk/ta_dev_kit.mk:8: .../export-ta_arm32/mk/conf.mk: No such file or directory
make[1]: *** No rule to make target '.../export-ta_arm32/mk/conf.mk'.  Stop.
make[1]: Leaving directory '.../optee_os/ta_services/keymaster'
ta_services/ta_services.mk:35: recipe for target 'ta_keymaster' failed
make: *** [ta_keymaster] Error 2
make: *** Waiting for unfinished jobs....

Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Fixes: c7542534816c ("mk: add ta_services.mk");
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ta_services/ta_services.mk
@@ -31,7 +31,7 @@ ta_keymaster_dir := keymaster
 
 all: ta_keymaster
 .PHONY: ta_keymaster
-ta_keymaster:
+ta_keymaster: ta_dev_kit
 	$(MAKE) -C ta_services/$(ta_keymaster_dir) $(ta_keymaster_flags) \
 		O=$(CURDIR)/$(out-dir)/ta_services/$(ta_keymaster_dir)
 