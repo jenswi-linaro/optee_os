jenswi-linaro/optee_os

commit 2ccfcde23f43f44e5363a8e370ea4f71e9c32d2b

ta_services: add shell of keymaster TA
Adds a shell of keymaster TA to be used to assist an OP-TEE based
keymaster implementation.

Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ta_services/keymaster/km.c
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2018, Linaro Limited */
+
+#include <tee_internal_api.h>
+
+#include "km.h"
+
+TEE_Result km_add_rng_entropy(const void __unused *buf, size_t __unused blen)
+{
+	/* Stubbed until the system PTA is available */
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}

ta_services/keymaster/Makefile
@@ -1,0 +1,18 @@
+# The UUID for the Trusted Application
+BINARY=d36b30c7-5a5f-472c-af97-7f38a2edab7d
+
+ifdef TA_CROSS_COMPILE
+CROSS_COMPILE ?= $(TA_CROSS_COMPILE)
+endif
+export CROSS_COMPILE
+
+CFG_TEE_TA_LOG_LEVEL ?= 2
+CPPFLAGS += -DCFG_TEE_TA_LOG_LEVEL=$(CFG_TEE_TA_LOG_LEVEL)
+
+-include $(TA_DEV_KIT_DIR)/mk/ta_dev_kit.mk
+
+ifeq ($(wildcard $(TA_DEV_KIT_DIR)/mk/ta_dev_kit.mk), )
+clean:
+	@echo 'Note: $$(TA_DEV_KIT_DIR)/mk/ta_dev_kit.mk not found, cannot clean TA'
+	@echo 'Note: TA_DEV_KIT_DIR=$(TA_DEV_KIT_DIR)'
+endif

ta_services/keymaster/entry.c
@@ -1,0 +1,54 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2018, Linaro Limited */
+
+#include <keymaster_ta.h>
+#include <tee_internal_api.h>
+
+#include "km.h"
+
+static TEE_Result add_rng_entropy(uint32_t pt, TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	return km_add_rng_entropy(params[0].memref.buffer,
+				  params[0].memref.size);
+}
+
+TEE_Result TA_CreateEntryPoint(void)
+{
+	return TEE_SUCCESS;
+}
+
+void TA_DestroyEntryPoint(void)
+{
+}
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t __unused pt,
+				    TEE_Param __unused params[4],
+				    void __unused **session)
+{
+	return TEE_SUCCESS;
+}
+
+void TA_CloseSessionEntryPoint(void __unused *sess)
+{
+}
+
+TEE_Result TA_InvokeCommandEntryPoint(void __unused *sess, uint32_t cmd,
+				      uint32_t pt,
+				      TEE_Param params[TEE_NUM_PARAMS])
+{
+	switch (cmd) {
+	case KEYMASTER_CMD_ADD_RNG_ENTROPY:
+		return add_rng_entropy(pt, params);
+	default:
+		EMSG("Command ID 0x%x is not supported", cmd);
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+}

ta_services/keymaster/include/keymaster_ta.h
@@ -1,0 +1,20 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2018, Linaro Limited */
+
+#ifndef __KEYMASTER_TA_H
+#define __KEYMASTER_TA_H
+
+#define TA_KEYMASTER_UUID { 0xd36b30c7, 0x5a5f, 0x472c, \
+			    {0xaf, 0x97, 0x7f, 0x38, 0xa2, 0xed, 0xab, 0x7d } }
+
+/*
+ * Add (re-seed) caller-provided entropy to the RNG pool. Keymaster
+ * implementations need to securely mix the provided entropy into their
+ * pool, which also must contain internally-generated entropy from a
+ * hardware random number generator.
+ *
+ * in	params[0].memref: entropy input data
+ */
+#define KEYMASTER_CMD_ADD_RNG_ENTROPY	0
+
+#endif /*__KEYMASTER_TA_H*/

ta_services/keymaster/km.h
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2018, Linaro Limited */
+
+#ifndef LOCAL_KM_H
+#define LOCAL_KM_H
+
+#include <tee_internal_api.h>
+
+TEE_Result km_add_rng_entropy(const void *buf, size_t blen);
+
+#endif /*LOCAL_KM_H*/

ta_services/keymaster/private_include/user_ta_header_defines.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2018, Linaro Limited */
+
+#ifndef __USER_TA_HEADER_DEFINES_H
+#define __USER_TA_HEADER_DEFINES_H
+
+#include <keymaster_ta.h>
+
+#define TA_UUID				TA_KEYMASTER_UUID
+
+#define TA_FLAGS			(TA_FLAG_SINGLE_INSTANCE | \
+					 TA_FLAG_MULTI_SESSION)
+
+#define TA_STACK_SIZE			(16 * 1024)
+#define TA_DATA_SIZE			(16 * 1024)
+
+#define TA_CURRENT_TA_EXT_PROPERTIES \
+    { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
+        "AOSP Keymaster services Trusted Application" }, \
+    { "gp.ta.version", USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0 } }
+
+#endif /*__USER_TA_HEADER_DEFINES_H*/

ta_services/keymaster/sub.mk
@@ -1,0 +1,4 @@
+global-incdirs-y += include
+global-incdirs-y += private_include
+srcs-y += entry.c
+srcs-y += km.c

commit 99a78aeb3a5ec166fd75abc468d5c3f1b9e34fa5

keymaster: km.c: fix SPDX comment
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Fixes: 2ccfcde23f43 ("ta_services: add shell of keymaster TA")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ta_services/keymaster/km.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: BSD-2-Clause */
+// SPDX-License-Identifier: BSD-2-Clause
 /* Copyright (c) 2018, Linaro Limited */
 
 #include <pta_system.h>