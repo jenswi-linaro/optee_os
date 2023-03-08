cc4cf695a6b8299ae82d54449c6007aa5323871f
Jens Wiklander <jens.wiklander@linaro.org>
2023-03-08 12:20:27 +0100

    core: kernel: add printf() and puts()
    Adds printf() and puts() to trace_ext.c, enabled at level
    TRACE_PRINTF_LEVEL (TRACE_ERROR) just as for TAs. This helps when
    integrating a third-party library that is using printf() for logging.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/trace_ext.c
+++ core/kernel/trace_ext.c
@@ -3,6 +3,7 @@
  * Copyright (c) 2014, Linaro Limited
  */
 #include <stdbool.h>
+#include <stdio.h>
 #include <trace.h>
 #include <console.h>
 #include <kernel/misc.h>
@@ -61,3 +62,73 @@ int trace_ext_get_core_id(void)
 	else
 		return -1;
 }
+
+
+#if TRACE_LEVEL > 0
+/*
+ * printf and puts - stdio printf support
+ *
+ * 'printf()' and 'puts()' traces have the 'info' trace level.
+ */
+int printf(const char *fmt, ...)
+{
+	char to_format[MAX_PRINT_SIZE];
+	va_list ap;
+	int s;
+
+	if (trace_get_level() < TRACE_PRINTF_LEVEL)
+		return 0;
+
+	va_start(ap, fmt);
+	s = vsnprintf(to_format, sizeof(to_format), fmt, ap);
+	va_end(ap);
+
+	if (s < 0)
+		return s;
+
+	trace_ext_puts(to_format);
+
+	return s;
+}
+
+int puts(const char *str)
+{
+	if (trace_get_level() >= TRACE_PRINTF_LEVEL) {
+		trace_ext_puts(str);
+		trace_ext_puts("\n");
+	}
+	return 1;
+}
+
+int putchar(int c)
+{
+	char str[2] = { (char)c, '\0' };
+
+	if (trace_get_level() >= TRACE_PRINTF_LEVEL)
+		trace_ext_puts(str);
+	/*
+	 * From the putchar() man page:
+	 * "fputc(), putc() and putchar() return the character written as an
+	 * unsigned char cast to an int or EOF on error."
+	 */
+	return (int)(unsigned char)c;
+}
+
+#else
+
+int printf(const char *fmt __unused, ...)
+{
+	return 0;
+}
+
+int puts(const char *str __unused)
+{
+	return 0;
+}
+
+int putchar(int c)
+{
+	return (int)(unsigned char)c;
+}
+
+#endif
