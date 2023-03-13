b30ec0750902dba37b05aa123b913647738ee2ac
Jens Wiklander <jens.wiklander@linaro.org>
2023-03-13 15:03:48 +0100

    core: ibmtss: add internal errno.h
    Adds an internal errno.h expected to exist by a few of the .c files.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/lib/ibmtss/internal_inc/errno.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2023, Linaro Limited
+ */
+
+#ifndef __ERRNO_H
+#define __ERRNO_H
+
+/*
+ * No defintions are needed at the moment, but the file is expected to
+ * exist.
+ */
+
+#endif /* __ERRNO_H */
